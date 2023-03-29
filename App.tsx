c7de129576318e7a83ca72c037aa5dd18df02389
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-03-28 21:27:33 -0300

    config: added details to run app on Android device with Expo Dev Client and OneSignal

--- App.tsx
+++ App.tsx
@@ -1,4 +1,5 @@
 import { StatusBar } from 'react-native';
+import { OneSignal } from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -8,6 +9,8 @@ import { THEME } from './src/theme';
 import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
+
+OneSignal.initialize("63301e63-6330-4ebd-b579-d3b36d90f616");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
