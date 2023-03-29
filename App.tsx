04ee4d180bc276b7a351f40e614fc0b361a4cc63
Atirson Fabiano Barbosa de Oliveira <fabiano.oliveira2213@gmail.com>
2023-03-29 15:08:20 -0300

    bug: device don't receive the notification

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'react-native';
-import { OneSignal } from 'react-native-onesignal';
+import OneSignal from 'react-native-onesignal';
 import { NativeBaseProvider } from 'native-base';
 import { useFonts, Roboto_400Regular, Roboto_700Bold } from '@expo-google-fonts/roboto';
 
@@ -10,7 +10,7 @@ import { Loading } from './src/components/Loading';
 
 import { CartContextProvider } from './src/contexts/CartContext';
 
-OneSignal.initialize("63301e63-6330-4ebd-b579-d3b36d90f616");
+OneSignal.setAppId("63301e63-6330-4ebd-b579-d3b36d90f616");
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
